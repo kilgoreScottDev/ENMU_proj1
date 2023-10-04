7fb5aadc8c99142a381d667ec2638732f2130c2c
ScottTpirate <44659733+ScottTpirate@users.noreply.github.com>
2023-10-03 21:01:54 -0600

    Create kilgore.scott.java
    update

---
+++ Upload_here/kilgore.scott.java
@@ -0,0 +1,17 @@
+public class TestErrors {
+
+    public static void main(String[] args) {
+        int num1 = 5
+        int num2 = "10";
+        
+        System.out.println("Sum is: " + add(num1, num2));
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
